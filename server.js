7a837dd2b9be955fb661475f4ce854c416f670b5
aditya78230 <adityajadhav14143@gmail.com>
2025-01-31 23:39:40 +0530

    Added main server file

---
+++ server.js
@@ -0,0 +1,21 @@
+const express= require('express');
+const app = express();
+const api = require("./routes/que");
+const bodyParser = require('body-parser');
+const admin = require("./routes/admin")
+
+
+app.use(bodyParser.json());
+
+app.use('/api',api)
+app.use('/admin',admin)
+
+app.listen(3000,()=>{
+    console.log("Server on");
+})
+
+
+
+//redis
+//translate
+//answer format
